d5f1db657f7a3cfe580b2dda2627f7fde96ec624
NipeH <nipe_h@hotmail.com>
2020-04-22 20:57:47 +0300

    postTicketData & postOrderData

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
 
-  async postData(){
+  async postTicketData(){
+  
+    let auth = btoa('niilo:salasana');
+    let result = await fetch("https://ticketguru.herokuapp.com/api/events/1/tickets", {
+    method : 'post',
+    mode: 'cors',
+    cache : 'no-cache',
+    credentials : 'same-origin',
+    headers : {
+        'Accept': 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Basic ' + auth,
+    },
+    body : JSON.stringify({
+        
+      pcs: '3',
+      orderid: '14',
+      tickettypeid: '4',
+
+    })
+
+
+  });
+
+  console.log(result);
+}
+
+
+  async postOrderData(){
   
         let auth = btoa('niilo:salasana');
-        let result = await fetch("https://ticketguru.herokuapp.com/api/tickets/", {
+        let result = await fetch("https://ticketguru.herokuapp.com/api/orders/", {
         method : 'post',
         mode: 'cors',
         cache : 'no-cache',
@@ -18,9 +46,8 @@ class App extends Component {
             'Authorization' : 'Basic ' + auth,
         },
         body : JSON.stringify({
-            eventid: '1',
-            tickettypeid: '6',
-            isvalid: 'true',
+            orderid: '1',
+           
 
         })
 
@@ -36,7 +63,9 @@ class App extends Component {
 
         <div className="App">
 
-          <button onClick={() => this.postData()}>Post ticket order data</button>
+          <button onClick={() => this.postOrderData()}>Post order data</button>
+
+          <button onClick={() => this.postTicketData()}>Post ticket data</button>
 
         </div>
 
@@ -48,3 +77,33 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+/* async postData(){
+  
+      
+  let auth = btoa('niilo:salasana');
+  let result = await fetch("https://ticketguru.herokuapp.com/api/tickets/", {
+  method : 'post',
+  mode: 'cors',
+  cache : 'no-cache',
+  credentials : 'same-origin',
+  headers : {
+      'Accept': 'application/json',
+      'Content-Type' : 'application/json',
+      'Authorization' : 'Basic ' + auth,
+  },
+  body : JSON.stringify({
+      eventid: '1',
+      tickettypeid: '6',
+      isvalid: 'true',
+
+  })
+
+
+});
+
+console.log(result);
+} */
